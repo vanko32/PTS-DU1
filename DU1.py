e9f05b0dc486c866a9bc41406790598fab0419ed
vanko32 <62019438+vanko32@users.noreply.github.com>
2020-03-10 22:32:29 +0100

    Create DU1.py
    Vytvorenie file-u, inicializacia metod - bez funkcionality

---
+++ DU1.py
@@ -0,0 +1,51 @@
+from pyrsistent import s
+
+
+class Relation:
+    def __init__(self):
+        pass
+
+    def contains(self):
+        pass
+    
+    def add(self):
+        pass
+
+    def remove(self):
+         pass
+
+    def union(self):
+       pass
+
+    def intersection(self):
+        pass
+
+    def subtraction(self):
+        pass
+
+    def inverse(self):
+        pass
+
+    def composition(self):
+        pass
+
+    def isReflexive(self):
+        pass
+
+    def isSymmetric(self):
+        pass
+
+    def isTransitive(self):
+        pass
+
+    def reflexiveTransitiveClosure(self):
+        pass
+
+
+def get_relation_class(set):
+    pass
+
+
+if __name__ == "__main__":
+
+
